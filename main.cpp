d88c83e95c159a74b70c2149132de22062cfdda3
ChristopherCollins67 <CDMxSB@gmail.com>
2025-09-03 15:45:36 +0000

    BMI Christopher Collins

--- main.cpp
+++ main.cpp
@@ -2,7 +2,15 @@
 using namespace std;
 
 int main() {
- 
+    int height=61;
+    int weight=130;
+   
+    double kg= weight * 0.453592;
+    double meter= height * 0.0254;
 
+    double BMI= kg / (meter*meter);
+
+
+ cout << "The BMI of this person is : "<< BMI << "."<< endl;
 
 }
